9d82d124e5603e0e36c9c91ed24fdc5c9325ae41
driodmaster92 <adityasureka92@gmail.com>
2013-07-12 18:47:06 +0530

    Update and rename slim.mk to illusion.mk
    inherit various illusion configs and setting

--- illusion.mk
+++ illusion.mk
@@ -1,5 +1,5 @@
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/slim/config/gsm.mk)
+$(call inherit-product, vendor/illusion/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := n7100
@@ -8,25 +8,25 @@ PRODUCT_RELEASE_NAME := n7100
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common Slim stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+# Inherit some common Illusion stuff.
+$(call inherit-product, vendor/illusion/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/n7100/full_n7100.mk)
 
 # Inherit torch settings
-$(call inherit-product, vendor/slim/config/common_ledflash.mk)
+$(call inherit-product, vendor/illusion/config/common_ledflash.mk)
 
 # Inherit device settings
-$(call inherit-product, vendor/slim/config/common_sgs.mk)
+$(call inherit-product, vendor/illusion/config/common_sgs.mk)
 
 PRODUCT_COPY_FILES +=  \
-    vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip \
+    vendor/illusion/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip \
     device/samsung/n7100/69cpuinit:system/etc/init.d/69cpuinit
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := n7100
-PRODUCT_NAME := slim_n7100
+PRODUCT_NAME := illusion_n7100
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-N7100
 PRODUCT_MANUFACTURER := samsung
